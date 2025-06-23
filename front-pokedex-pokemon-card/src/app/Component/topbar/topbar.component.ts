3bdaee8c6e331fc062c0e08e9683f4064843fe92
Baptiste <baptiste.dombry12@gmail.com>
2025-06-23 18:44:34 +0200

    ajout homme button

--- front-pokedex-pokemon-card/src/app/Component/topbar/topbar.component.ts
+++ front-pokedex-pokemon-card/src/app/Component/topbar/topbar.component.ts
@@ -2,6 +2,7 @@ import { Component, output } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -14,8 +15,15 @@ export class TopbarComponent {
 
   isSideMenuVisible = output<boolean>();
 
+  constructor(private router: Router) { }
+
   handleMenuClick()
   {
     this.isSideMenuVisible.emit(true);
+  }
+
+  handleHomeClick()
+  {
+    this.router.navigate(['/'])
   }
 }
