e3ff2e5279513015457ad69f42214d323e6367d0
Baptiste <baptiste.dombry12@gmail.com>
2025-06-25 16:15:41 +0200

    environment prod

--- front-pokedex-pokemon-card/src/environments/environment.prod.ts
+++ front-pokedex-pokemon-card/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     googleClientId: '770704774312-vgs0qqkpm6vk2oedraqg2n1dvn4ed4le.apps.googleusercontent.com',
-    apiUrl: "http://localhost:5001",
+    apiUrl: "https://pokedexapp.fr/api",
     localStorageTokenString: 'APIToken',
 }
