bdc3d88821a9280c431f380f181414d605e8dedd
Baptiste <baptiste.dombry12@gmail.com>
2025-09-09 16:16:15 +0200

    Ajout logo type de pokedex

--- front-pokedex-pokemon-card/src/app/Component/home/home.component.ts
+++ front-pokedex-pokemon-card/src/app/Component/home/home.component.ts
@@ -4,8 +4,9 @@ import { AuthService } from '../../Services/auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { PokedexType } from '../../Models/pokedex';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -23,10 +24,29 @@ export class HomeComponent implements OnInit {
   Role = Role;
   PokedexType = PokedexType;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'zarbi',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/zarbi.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'pokedex',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/pokedex_icon.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'loupe',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/loupe_pokeball.svg')
+    );
+  }
 
   ngOnInit(): void {
     this.authService.user$.subscribe((u) => {
+      console.log(u);
       this.currentUser = u
       this.titlePseudo = u?.pseudo ? u.pseudo : u?.email ?? ""
     });
@@ -64,5 +84,18 @@ export class HomeComponent implements OnInit {
   handlePanelAdmin()
   {
     this.router.navigate(['panel-admin']);
+  }
+
+  getIconPokedex(type: PokedexType): string {
+    switch (type) {
+      case PokedexType.LivingDex:
+        return 'pokedex';
+      case PokedexType.SpecificPokemonsDex:
+        return 'loupe';
+      case PokedexType.ZarbiDex:
+        return 'zarbi';
+      default:
+        return 'help_outline';
+    }
   }
 }
